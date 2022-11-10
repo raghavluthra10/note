cfd7cdcf6254f0b1aadd35026a16d885578ae6b4
Raghav Luthra <raghavluthra1@gmail.com>
2022-11-11 02:52:52 +0530

    added crud operations for todos

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 const Koa = require("koa");
 const port = 8000;
+const { router } = require("./routes");
+const bodyParser = require("koa-bodyparser");
+
 const app = new Koa();
 
-const { router } = require("./routes");
-
 const { db } = require("../database");
+
+app.use(bodyParser({ enableTypes: ["json", "text"] }));
 
 // add db access to context prototype so that it is available throughout the app
 app.context.db = db;
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,11 +2,99 @@ const Router = require("@koa/router");
 
 const router = new Router();
 
-router.get("/", (ctx) => {
-   ctx.body = "welcome to home page";
+router.redirect("/login", "/signin");
+
+router.get("/todos", async (ctx) => {
+   try {
+      const data = await ctx.db("todo");
+      console.log("data", data);
+
+      ctx.status = 200;
+      ctx.body = data;
+   } catch (error) {
+      ctx.status = 500;
+      ctx.body = "Internal server error";
+      console.log(error);
+   }
 });
 
-router.redirect("/login", "/signin");
+router.get("/todo/:id", async (ctx) => {
+   try {
+      const id = ctx.params.id;
+      const data = await ctx.db("todo").where({
+         id: id,
+      });
+      ctx.status = 200;
+      ctx.body = data;
+   } catch (error) {
+      console.log(error);
+      ctx.status = 500;
+      ctx.body = "Internal server error";
+   }
+});
+
+router.post("/todo", async (ctx) => {
+   try {
+      const data = await JSON.parse(ctx.request.body);
+
+      await ctx.db("todo").insert(data);
+
+      ctx.body = data;
+      ctx.status = 200;
+   } catch (error) {
+      console.log(error);
+      ctx.status = 500;
+      ctx.body = "Internal server error";
+   }
+});
+
+router.delete("/todo/:id", async (ctx) => {
+   try {
+      const id = ctx.params.id;
+      await ctx.db("todo").where("id", id).del();
+      ctx.body = "Deleted successfully" + " " + id;
+      ctx.status = 200;
+   } catch (error) {
+      console.log(error);
+      ctx.status = 500;
+      ctx.body = "Internal server error";
+   }
+});
+
+router.put("/todo/:id", async (ctx) => {
+   try {
+      const id = ctx.params.id;
+      const recievedBody = await JSON.parse(ctx.request.body);
+
+      const title = recievedBody.title;
+      const description = recievedBody.description;
+      const completed = recievedBody.completed;
+
+      const todoToBeUpdated = {};
+
+      if (title) {
+         todoToBeUpdated.title = title;
+      }
+
+      if (description) {
+         todoToBeUpdated.description = description;
+      }
+
+      if (completed) {
+         todoToBeUpdated.completed = true;
+      } else if (completed === false) {
+         todoToBeUpdated.completed = false;
+      }
+
+      await ctx.db("todo").where({ id: id }).update(todoToBeUpdated);
+      ctx.status = 200;
+      ctx.body = "Todo updated successfully!";
+   } catch (error) {
+      console.log(error);
+      ctx.status = 500;
+      ctx.body = "Internal server error";
+   }
+});
 
 module.exports = {
    router,
